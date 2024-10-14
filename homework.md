73a6e387cdffe1c4aa258485669f29f2585e580c
manorath901 <learn4833@gmail.com>
2024-10-14 23:06:52 +0530

    create a server

---
+++ homework.md
@@ -0,0 +1,8 @@
+create a  repo
+intialoze the project npm init
+node module packagejson and lock
+install express npm i express
+create a server
+listen to port 7777
+write request handeler /test ,/hello
+install nodemon and updtae scripy  in package.json
---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const app=express()
+app.use("/",(req,res)=>{  //request,response
+    res.send("hello namste   from the dashboard")
+    }) 
+app.use("/hello",(req,res)=>{  //request,response
+    res.send("hello hello hello")
+    }) 
+app.use("/test",(req,res)=>{  //request,response
+res.send("hello from the server")
+}) //request handeler srver is lsiting over here
+app.listen(7777,()=>{
+    console.log("server is successfully listing on port 7777")
+}) //created a server 
+//nodemon install kro sudo npm i -g nodemon
