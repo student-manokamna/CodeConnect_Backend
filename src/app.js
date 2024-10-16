1d696855527e308db55c5288bdba232f50c19e83
manorath901 <learn4833@gmail.com>
2024-10-16 15:00:05 +0530

    changed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express=require('express');
 const app=express() //INSANCE OF EXPRESS
 const {adminAuth,userAuth}=require("./middlewares/auth")
+app.get('/getuserdata',(req,res)=>{
+    try{
+        throw new Error("fdgd");
+        res.send('user data sent')
+    }
+    catch(err){
+        res.status(500).send("something went wrong contact the team")// ihave send request do not get further 
+    }
+   
+    
+})
+app.use('/',(err,req,res,next)=>{ //wild card thing matches all route err should be first paraametet 
+if(err){ 
+    //log your error
+
+    res.status(500).send("something went wrong") //now ui handle gracefully do no tshow random meesaget o user 
+}
+})
 // app.use("/",(req,res)=>{  //request,response
 //     res.send("hello namste   from the dashboard") 
 //     }) 
@@ -20,28 +38,28 @@ const {adminAuth,userAuth}=require("./middlewares/auth")
 //     res.send("data successfully saved to database")
 // })
 //handle auth middleware for all get post request
-app.use("/admin",adminAuth) //called only for /admin
-// app.use("/user",userAuth)  // i have only on epath so write it isode 
-app.post("/user/login",(req,res)=>{
-    res.send("user logged in succewssfullly") //no need of authetication so no put userauth
-})
-app.get('/user/data',userAuth,(req,res)=>{
-    res.send("user data sent")
-})
-app.get('/admin/getalldata',(req,res)=>{
-    res.send("all data sent")
-})
-app.get('/admin/deletealldata',(req,res)=>{
-    // const token="xyz"  //req.body?.token
-    // const isAdminAuthrised=token==="xyz"
-    // // console.log("dsaven data to dtatabase")
-    // if(isAdminAuthrised){
-    res.send("all data sent")
-    // }
-    // else{
-    //     res.status(401).send("unautrisedn request")
-    // } upar /admin me hi likg diya logic
-})
+// app.use("/admin",adminAuth) //called only for /admin
+// // app.use("/user",userAuth)  // i have only on epath so write it isode 
+// app.post("/user/login",(req,res)=>{
+//     res.send("user logged in succewssfullly") //no need of authetication so no put userauth
+// })
+// app.get('/user/data',userAuth,(req,res)=>{
+//     res.send("user data sent")
+// })
+// app.get('/admin/getalldata',(req,res)=>{
+//     res.send("all data sent")
+// })
+// app.get('/admin/deletealldata',(req,res)=>{
+//     // const token="xyz"  //req.body?.token
+//     // const isAdminAuthrised=token==="xyz"
+//     // // console.log("dsaven data to dtatabase")
+//     // if(isAdminAuthrised){
+//     res.send("all data sent")
+//     // }
+//     // else{
+//     //     res.status(401).send("unautrisedn request")
+//     // } upar /admin me hi likg diya logic
+// })
 // app.delete('/user',(req,res)=>{
     
 //     res.send("delted succesfully")
